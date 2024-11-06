421453afc5ef2f76dd3960469217c10fddbc5cab
anjuE9431 <anjuanilkumar292@gmail.com>
2024-11-06 15:42:34 +0530

    Delete week5 directory

--- week5/circlefun.py
+++
@@ -1,4 +0,0 @@
-import math
-area_of_circle = lambda radius: math.pi * radius ** 2
-radius=int(input("Enter the radius: "))
-print(f"Area of the circle with radius {radius} is {area_of_circle(radius)}")
--- week5/lambda.py
+++
@@ -1,3 +0,0 @@
-cube = lambda x: x ** 3
-number =int(input("enter the number:"))
-print(f"The cube of {number} is {cube(number)}")
